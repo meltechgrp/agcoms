2a537ea5f68a432bbdd9d12a9dcab3fddf8e4751
Humphrey Joshua <joshuahumphrey579@gmail.com>
2024-09-24 12:44:28 +0100

    correct database path

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const schema = z.object({
-	DATABASE_URL: z.string(),
+	POSTGRES_URL: z.string(),
 	SMTP_HOST: z.string(),
 	SMTP_PORT: z.string(),
 	SMTP_USER: z.string(),
