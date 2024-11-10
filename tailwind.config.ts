c60f57f40df403cb8df29226f58d493096a0b20e
Humphrey Joshua <joshuahumphrey579@gmail.com>
2024-11-10 19:39:07 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 2px)',
 				md: 'calc(var(--radius) - 4px)',
 			},
 			keyframes: {
