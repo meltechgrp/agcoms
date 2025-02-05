393457469735c2334871c9ceaef23761f1a78959
Moses Monday <mondaymoses.ict@gmail.com>
2025-02-05 10:27:29 +0100

    added coming soon

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,18 +1,18 @@
-import Footer from '@/components/home/footer';
-import Header from '@/components/shared/header';
+import Footer from "@/components/home/footer";
+import Header from "@/components/shared/header";
 
 export default function UserLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<div className="flex min-h-screen w-full shadow-md flex-col overflow-x-hidden bg-white font-[family-name:var(--font-geist-sans)] mx-auto xl:max-w-[1350px]">
-			<Header />
-			<main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40">
-				<div className="mx-auto grid w-full items-start gap-6">{children}</div>
-				<Footer />
-			</main>
-		</div>
-	);
+  return (
+    <div className="flex min-h-screen w-full shadow-md flex-col overflow-x-hidden bg-white font-[family-name:var(--font-geist-sans)] mx-auto xl:max-w-[1350px]">
+      {/* <Header /> */}
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40">
+        <div className="mx-auto grid w-full items-start gap-6">{children}</div>
+        {/* <Footer /> */}
+      </main>
+    </div>
+  );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,26 +1,26 @@
-import Banner from '@/components/home/banner';
-import Choose from '@/components/home/choose';
-import Connection from '@/components/home/connection';
-import FeaturedProducts from '@/components/home/featured-product';
-import MidNavigation from '@/components/home/mid-navigation';
-import Request from '@/components/home/request';
-import Solution from '@/components/home/solution';
-import RequestForm from './requests/components/request-form';
-import prisma from '@/lib/prisma';
-import FloatingCards from '@/components/home/floating-cards';
-import { getProductImages } from '@/lib/actions';
+import Banner from "@/components/home/banner";
+import Choose from "@/components/home/choose";
+import Connection from "@/components/home/connection";
+import FeaturedProducts from "@/components/home/featured-product";
+import MidNavigation from "@/components/home/mid-navigation";
+import Request from "@/components/home/request";
+import Solution from "@/components/home/solution";
+import RequestForm from "./requests/components/request-form";
+import prisma from "@/lib/prisma";
+import FloatingCards from "@/components/home/floating-cards";
+import { getProductImages } from "@/lib/actions";
 
 interface Props {
-	searchParams: {
-		request?: string;
-	};
+  searchParams: {
+    request?: string;
+  };
 }
 
 export default async function Home({ searchParams }: Props) {
-	const data = await getProductImages();
-	return (
-		<div className="grid min-h-screen gap-4">
-			<FeaturedProducts />
+  const data = await getProductImages();
+  return (
+    <div className="grid min-h-screen gap-4">
+      {/* <FeaturedProducts />
 			<Banner />
 			<MidNavigation />
 			<Solution />
@@ -28,7 +28,10 @@ export default async function Home({ searchParams }: Props) {
 			<FloatingCards data={data} />
 			<Choose />
 			<Request />
-			<RequestForm open={!!searchParams?.request} />
-		</div>
-	);
+			<RequestForm open={!!searchParams?.request} /> */}
+      <div className=" text-4xl md:text-7xl w-full font-black h-full flex justify-center items-center">
+        <p>Coming Soon</p>
+      </div>
+    </div>
+  );
 }
