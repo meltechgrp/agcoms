714e96c38248ddb221a28c21be8f877bb80a61d7
Nkanu sunday <protechnologies332@gmail.com>
2026-03-26 16:33:13 +0100

    modified equipment section in home page

--- src/components/home/floating-cards.tsx
+++ src/components/home/floating-cards.tsx
@@ -35,10 +35,9 @@ export default function FloatingCards(props: Props) {
         </h1>
         <p className="text-sm lg:text-base w-full lg:w-[50%] text-center">
           Explore our comprehensive range of high-performance equipment tailored
-          for agriculture, construction, forestry, golf, and turf maintenance
-          across Africa and the Middle East. Built for durability and
-          efficiency, our machines are designed to meet the unique demands of
-          varied terrains and challenging conditions.
+          for agriculture across Africa and the Middle East. Built for
+          durability and efficiency, our machines are designed to meet the
+          unique demands of varied terrains and challenging conditions.
         </p>
       </div>
       <InfiniteMovingImages
--- src/stores/use-nav-store.ts
+++ src/stores/use-nav-store.ts
@@ -1,73 +1,73 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export interface Basic {
-	name: string;
-	slug: string;
-	link: string;
+  name: string;
+  slug: string;
+  link: string;
 }
 export type ProCats = {
-	name: string;
-	slug: string;
-	link: string;
+  name: string;
+  slug: string;
+  link: string;
 };
 export type State = {
-	pages: Basic[];
-	product: ProCats[];
+  pages: Basic[];
+  product: ProCats[];
 };
 const initialState: State = {
-	pages: [
-		{ name: 'Equipment', slug: 'equipment', link: '/equipment' },
-		{
-			name: 'Parts & Services',
-			slug: 'parts',
-			link: '/parts',
-		},
-		{ name: 'Request for Quotation', slug: 'requests', link: '/requests' },
-		{ name: 'About Us', slug: 'about', link: '/about' },
-		{ name: 'Contact Us', slug: 'contact', link: '/contact' },
-	],
-	product: [
-		{
-			name: 'Agriculture',
-			slug: 'agriculture',
-			link: '/equipment/agriculture',
-		},
-		{
-			name: 'Construction',
-			slug: 'construction',
-			link: '/equipment/construction',
-		},
-		// {
-		// 	name: 'Lawn & Garden',
-		// 	slug: 'lawn-garden',
-		// 	link: '/equipment/lawn-garden',
-		// },
-		{
-			name: 'Golf & Sports Turf',
-			slug: 'golf-sports-turf',
-			link: '/equipment/golf-sports-turf',
-		},
-		{
-			name: 'Forestry',
-			slug: 'forestry',
-			link: '/equipment/forestry',
-		},
-	],
+  pages: [
+    { name: "Equipment", slug: "equipment", link: "/equipment" },
+    {
+      name: "Parts & Services",
+      slug: "parts",
+      link: "/parts",
+    },
+    { name: "Request for Quotation", slug: "requests", link: "/requests" },
+    { name: "About Us", slug: "about", link: "/about" },
+    { name: "Contact Us", slug: "contact", link: "/contact" },
+  ],
+  product: [
+    {
+      name: "Agriculture",
+      slug: "agriculture",
+      link: "/equipment/agriculture",
+    },
+    // {
+    // 	name: 'Construction',
+    // 	slug: 'construction',
+    // 	link: '/equipment/construction',
+    // },
+    // {
+    // 	name: 'Lawn & Garden',
+    // 	slug: 'lawn-garden',
+    // 	link: '/equipment/lawn-garden',
+    // },
+    {
+      name: "Golf & Sports Turf",
+      slug: "golf-sports-turf",
+      link: "/equipment/golf-sports-turf",
+    },
+    // {
+    // 	name: 'Forestry',
+    // 	slug: 'forestry',
+    // 	link: '/equipment/forestry',
+    // },
+  ],
 };
 type StateAndActions = State;
 const useNavStore = create<StateAndActions>()(
-	persist(
-		(set, get) => ({
-			...initialState,
-		}),
-		{
-			name: 'agcom-storage',
-			partialize: (state) => ({
-				// products: state.product,
-			}),
-		}
-	)
+  persist(
+    (set, get) => ({
+      ...initialState,
+    }),
+    {
+      name: "agcom-storage",
+      partialize: (state) => ({
+        // products: state.product,
+      }),
+    },
+  ),
 );
 
 export default useNavStore;
